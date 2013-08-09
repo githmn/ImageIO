5c96c4ae95ee76d94eb0ec0e93a4162d27621e3f
Yuki Hamano <i@hmn.cc>
2013-08-09 22:52:47 +0900

    画像の送信まで

---
+++ ImageIOProject/src/client/ImageIOClient.java
@@ -0,0 +1,52 @@
+package client;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+import javax.imageio.ImageIO;
+
+public class ImageIOClient {
+
+	public static void main(String args[]) throws Exception{
+		new ImageIOClient().exec();
+	}
+	
+	public static final String SERVER_HOST = "192.168.244.139";
+	public static final int SERVER_PORT = 10514;
+	
+	private void exec() throws Exception{
+		
+		String srcDirPath = "./resources/client/src/";
+		File srcDir = new File(srcDirPath);
+		File srcFiles[] = srcDir.listFiles();
+		
+		for(File srcFile : srcFiles){
+			
+			Socket sock = null;
+			
+			try{
+			sock = new Socket(ImageIOClient.SERVER_HOST, ImageIOClient.SERVER_PORT);
+			
+			InputStream is = sock.getInputStream();
+			OutputStream os = sock.getOutputStream();
+			
+			BufferedImage srcImg = ImageIO.read(srcFile);
+			
+			ImageIO.write(srcImg, "bmp", os);
+			
+			}catch(Exception e){
+				e.printStackTrace();
+			}finally{
+				if(sock != null){
+					sock.close();
+				}
+			}
+			
+		}
+		
+	}
+	
+}
